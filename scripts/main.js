12a436b5976e9a0407d3a69b0ce4e11cd673fd92
thegeekynerd <techyeagle10@gmail.com>
2020-04-15 13:42:09 +0530

    New

---
+++ scripts/main.js
@@ -0,0 +1,9 @@
+// redirect = ()=>{
+//     window.location.href="construction.html"
+// }
+
+$(document).ready(function(){
+    $('.card').on('click ', function() {
+        window.location.href="construction.html"
+    });
+})
